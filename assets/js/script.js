ae99b9d5047a7d4628988f2d4dab19e60467224f
unknown <d0cando@yandex.ru>
2022-04-09 21:58:53 +0400

    add project

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// wow = new WOW(
+//     {
+//         animateClass: 'animated',
+//         offset:       100,
+//         callback:     function(box) {
+//         console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+//         }
+//     }
+// );
+// wow.init();
+new WOW().init();
+
+console.log('Web-Developer: AvoPro');
